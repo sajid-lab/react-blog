d9d2d3cf1a009732d3e228ea55ace89458dacff0
sajid-label <shaik.sajid2595@gmail.com>
2020-05-17 07:30:09 +0530

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-app.get('/', (req,res)=>{
-    res.send("Hello World")
+const config = require('./config/key')
+
+const { User } = require('./models/user')
+
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.post('/api/users/register', (req,res)=>{
+
+    const user = new User(req.body)
+
+    user.save((err, doc)=> {
+    if(err) return res.json ({ success: false,err })
+    res.status(200).json({
+        success: true,
+        userData: doc
+    })
+  })
 })
 
-mongoose.connect('mongodb+srv://sajidlab:Frontenddevelopment@123@react-blog-w4ltf.mongodb.net/test?retryWrites=true&w=majority',
+app.get('/', (req,res)=>{
+    res.json("Hello Hi Get Lost")
+})
+
+mongoose.connect(config.mongoURI,
 {useNewUrlParser: true}).then(()=>console.log("DB created"))
                         .catch(err=>console.error(err))
 app.listen(5000)
